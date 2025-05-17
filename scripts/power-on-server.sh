c4f3197324677e2e358f664bf8b0cf85dec9750c
OnyxJeff <j.may2702@gmail.com>
2025-05-17 14:10:54 -0400

    updated power-on-server.sh with current servers

--- scripts/power-on-server.sh
+++ scripts/power-on-server.sh
@@ -17,20 +17,18 @@ HOSTS=(
     192.168.1.1   # yggdrasil (Router)
     192.168.1.7   # Gladsheim (Pi-NAS [OMV])
     192.168.1.8   # Muninn (TrueNAS Scale)
-    192.168.1.10  # Muninn (TrueNAS Scale)
     192.168.1.14  # Aesir1 (Vital Services Cluster - Aesir 1)
-    192.168.1.17  # PotentPi1 (PotentPi 1 [PiHole & PiVPN])
-    192.168.1.18  # PotentPi2 (PotentPi 2 [Internal Dashboard & Unifi Controller])
-    192.168.1.19  # PotentPi3 (PotentPi 3 [Transmission Server])
-    192.168.1.20  # PotentPi4 (PotentPi 4 [Minecraft Server])
-    192.168.1.21  # PotentPi5 (PotentPi 5 [Home Assistant])
+    192.168.1.17  # Odin (PotentPi 1 [PiHole & PiVPN])
+    192.168.1.18  # Mimir (PotentPi 2 [Internal Dashboard & Unifi Controller])
+    192.168.1.19  # Loki (PotentPi 3 [Transmission Server])
+    192.168.1.20  # Midguard (PotentPi 4 [Minecraft Server])
+    192.168.1.21  # Frigg (PotentPi 5 [Home Assistant])
 )
 HOSTSDOWN=0
 MACADDRS=(
     38:f7:cd:c1:e3:cc # 192.168.1.1
     dc:a6:32:dc:46:2b # 192.168.1.7
     ac:1f:6b:b2:0d:9e # 192.168.1.8
-    ac:1f:6b:b2:0d:9f # 192.168.1.10
     f4:4d:30:6d:40:ab # 192.168.1.14
     e4:5f:01:67:86:e6 # 192.168.1.17
     e4:5f:01:0a:99:5c # 192.168.1.18
